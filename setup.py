59530babd56195d42fbfd24e6c60cbf91d7ace15
wangzhou <wangzhou8284@outlook.com>
2021-06-15 17:37:27 +0800

    use package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf8') as fh:
 
 setuptools.setup(
     name="mongo_log",
-    version="0.0.2",
+    version="0.0.3",
     author="Steven Wang",
     author_email="brightstar8284@icloud.com",
     description="use mongo db to log",
